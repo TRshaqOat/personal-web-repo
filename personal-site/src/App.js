4767a1e327953e77b7bd71e5cdd4d9ecfe79ac91
Achyut Patel <137658082+TRshaqOat@users.noreply.github.com>
2023-10-06 21:43:40 -0500

    Revert "Revert "Revert "undo"""
    This reverts commit 8eb01e279e73bd1c9de5214b4e795883420bdce1.

--- personal-site/src/App.js
+++ personal-site/src/App.js
@@ -1,21 +1,10 @@
-import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import lake from "./assets/lake.jpg";
-import meadow from "./assets/meadow.jpg";
-import waterfall from "./assets/waterfall.jpg";
 
 function App() {
-  const images = [lake, meadow, waterfall];
-  const [backgroundImage, setBackgroundImage] = useState(images[0]);
-
   return (
     <div className="App">
       <header className="App-header">
-        <div
-          className="bg-fixed bg-cover bg-center"
-          style={{ backgroundImage: `url(${backgroundImage})` }}
-        ></div>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
