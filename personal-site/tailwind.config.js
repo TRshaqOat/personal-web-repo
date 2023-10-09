ae2282e576f8961215c791181cebd0f0af726291
Achyut Patel <137658082+TRshaqOat@users.noreply.github.com>
2023-10-08 22:32:59 -0500

    now done!

--- personal-site/tailwind.config.js
+++ personal-site/tailwind.config.js
@@ -3,14 +3,7 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      keyframes: {
-        wiggle: {
-          "0%, 100%": { transform: "rotate(-3deg)" },
-          "50%": { transform: "rotate(3deg)" },
-        },
-      },
       animation: {
-        wiggle: "wiggle 1s ease-in-out infinite",
         "spin-slow": "spin 0.1s linear infinite",
       },
       colors: {
